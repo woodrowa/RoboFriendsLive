8f3ed609419a7d8c81d62fcd67ad190dc745b3af
Arien <woodrowa@bc.edu>
2020-07-24 17:04:10 -0400

    Updates

---
+++ precache-manifest.3cc1dd1c9aee4af36c4159087593ed2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370e20952df7683b6ce277a6b2df7696",
+    "url": "/RoboFriendsLive/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriendsLive/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e793e2adadc090630ba6",
+    "url": "/RoboFriendsLive/static/css/main.02858b54.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriendsLive/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriendsLive/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e793e2adadc090630ba6",
+    "url": "/RoboFriendsLive/static/js/main.31175d08.chunk.js"
+  },
+  {
+    "revision": "fa2a72687ae0b9577504",
+    "url": "/RoboFriendsLive/static/js/runtime-main.196ff2ba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsLive/static/media/SEGA.12a3db8b.woff"
+  }
+]);
